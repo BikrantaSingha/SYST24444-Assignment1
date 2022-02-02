da36b3db96a73c06c0d84665e03573b89118826b
BikrantaSingha <vikramsingh.webdev@gmail.com>
2022-02-02 05:12:22 -0500

    finished main html, started jquery

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(() => {
+	$("#main-form").submit(() => {
+		alert("Form submission successful!");
+	});
+
+	$("#main-form-save").click(() => {
+		alert("save successful!");
+	});
+})
